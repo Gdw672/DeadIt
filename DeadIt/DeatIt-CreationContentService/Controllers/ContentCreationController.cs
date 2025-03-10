61ee48fbb93c95207c9ff0b1bac1f01ea327ad05
Gdw672 <kirillmuravyev2@gmail.com>
2025-03-10 18:35:49 +0300

    Добавил базовые запрос-ответы во второй микросервис (CreationContentService)

---
+++ DeadIt/DeatIt-CreationContentService/Controllers/ContentCreationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeatIt_CreationContentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContentCreationController : ControllerBase
+    {
+        [HttpGet("GetAnswer")]
+        public string GetAnswer()
+        {
+            return "answer is delivered.";
+        }
+    }
+}
